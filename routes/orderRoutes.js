b35f1ae620a2a012990ea1e450cc9a8db1c68a93
SOWPARNIKA M <sowparnikasatheesh248@gmail.com>
2025-06-30 14:23:24 +0530

    first commit

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const orderRouter= express.Router();
 const orderController = require('../controllers/orderController');
+
 const { protect } = require('../middleware/authMiddleware');
 // Place an order
 orderRouter.post('/',protect, orderController.placeOrder);
 
 // Get user orders
-orderRouter.get('/user/:userId', authUser,orderController.getUserOrders);
+orderRouter.get('/user/:userId',orderController.getUserOrders);
 
 // Get all orders (admin)
 orderRouter.get('/', orderController.getAllOrders);
